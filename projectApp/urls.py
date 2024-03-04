8b9b1acd4403e650324f9daa2e13811741176f69
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-03-04 21:12:01 +0330

    template ok

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from projectApp.views import *
+from .views import index_view, about_view, contact_view, elements_view
 
 urlpatterns = [
-    path('', index_view),
-    path('about', about_view),
-    path('contact', contact_view)
-
+    path('', index_view, name='index'),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('elements/', elements_view, name='elements'),
 ]
--- projectApp/views.py
+++ projectApp/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse, JsonResponse
 
 
 def index_view(request):
-    return render(request, 'website\index.html')
+    return render(request, 'website/index.html')
 
 
 def about_view(request):
-    return render(request, 'website\about.html')
+    return render(request, 'website/about.html')
 
 
 def contact_view(request):
-    return render(request, 'website\contact.html')
+    return render(request, 'website/contact.html')
+
+
+def elements_view(request):
+    return render(request, 'website/elements.html')
