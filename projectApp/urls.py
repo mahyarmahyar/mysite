061fcc60bda768480ce3ab3c98e7382191c31734
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-03-06 19:34:55 +0330

    blog run

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index_view, about_view, contact_view, elements_view
 
+app_name = 'website'
+
 urlpatterns = [
     path('', index_view, name='index'),
     path('about/', about_view, name='about'),
