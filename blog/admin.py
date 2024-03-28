c431e55f0992b1b6d77a5b020f8b825b31d4bd39
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-03-28 18:15:39 +0330

    images

--- blog/admin.py
+++ blog/admin.py
@@ -7,13 +7,16 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     list_display = ('id', 'title', 'counted_views', 'status',
-                    'published_date', 'created_date')
-    list_filter = ('status', 'published_date',)
+                    'published_date', 'created_date', 'author')
+    list_filter = ('status', 'published_date', 'author')
 
     def counted_views(self, obj):
-        return obj.counted_view
+        return obj.counted_views
 
-    counted_views.admin_order_field = 'counted_view'
+    def author_name(self, obj):
+        return obj.author.username if obj.author else ""
+
+    counted_views.admin_order_field = 'counted_views'
     counted_views.short_description = 'Counted Views'
 
 
