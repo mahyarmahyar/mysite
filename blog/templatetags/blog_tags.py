26e94c27d8c05316eb6702149a65adefaac51b08
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-04-01 11:59:11 +0330

    tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from blog.models import Post
+from blog.models import Category
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts():
+    posts = Post.objects.filter(status=1).order_by('published_date')[:3]
+    return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories': cat_dict}
