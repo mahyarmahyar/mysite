1e8ad72ca11b0d6aba9c5a22e045ba3fa396da09
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-05-17 14:14:42 +0330

    coming soon

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -4,16 +4,16 @@ from .views import *
 app_name = 'projectApp'
 
 
-# urlpatterns = [
-# path('', coming_soon_view, name='coming_soon'),
-# This line will catch all other paths
-# path('&lt;path:unused_path&gt;/', coming_soon_view),
-# ]
-
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('about/', about_view, name='about'),
-    path('contact/', contact_view, name='contact'),
-    path('elements/', elements_view, name='elements'),
-    path('newsletter', newsletter_view, name='newsletter'),
+    path('', coming_soon_view, name='coming_soon'),
+    # This line will catch all other paths
+    path('&lt;path:unused_path&gt;/', coming_soon_view),
 ]
+
+# urlpatterns = [
+#    path('', index_view, name='index'),
+#    path('about/', about_view, name='about'),
+#    path('contact/', contact_view, name='contact'),
+#    path('elements/', elements_view, name='elements'),
+#    path('newsletter', newsletter_view, name='newsletter'),
+# ]
